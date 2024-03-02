6deed3b4e08ef19e260c96b9724ba7e6f8a94397
abhie53 <abhi35shinde@gmail.com>
2024-03-02 21:00:35 +0530

    update in Services

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -14,20 +14,20 @@ const Services = () => {
   return (
     <Layout>
       <Box sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
-        {ServiceList.map((menu) => (
+        {ServiceList.map((service) => (
           <Card sx={{ maxWidth: "390px", display: "flex", m: 2 }}>
             <CardActionArea>
               <CardMedia
                 sx={{ minHeight: "400px" }}
                 component={"img"}
-                src={menu.image}
-                alt={menu.name}
+                src={service.image}
+                alt={service.name}
               />
               <CardContent>
                 <Typography variant="h5" gutterBottom component={"div"}>
-                  {menu.name}
+                  {service.name}
                 </Typography>
-                <Typography variant="body2">{menu.description}</Typography>
+                <Typography variant="body2">{service.description}</Typography>
               </CardContent>
             </CardActionArea>
           </Card>
